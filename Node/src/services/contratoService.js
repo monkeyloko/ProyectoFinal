dbc0280b3b3fd3cf0436add3d8fae2cb2c3657b5
SANTIAGO DOFF <46873259@ort>
2023-07-31 11:58:08 -0300

    crud alquiler

--- Node/src/services/contratoService.js
+++ Node/src/services/contratoService.js
@@ -26,15 +26,15 @@ export class contratoService {
     createContrato = async (contrato) => {
         const pool = await sql.connect(config);
         const response = await pool.request()
-            .input('precio',sql.NChar, contrato?.precio ?? '')
-            .input('fechaAlquilado',sql.Int, contrato?.fechaAlquilado ?? 0)
-            .input('fechaDevolucion',sql.NChar, contrato?.fechaDevolucion ?? '')
-            .input('fkCliente',sql.NChar, contrato?.fkCliente ?? '')
-            .input('fkAuto',sql.Bit, contrato?.fkAuto ?? null)
-            .input('id_dañoEntrega',sql.Bit, contrato?.id_dañoEntrega ?? null)
-            .input('id_dañoDevolucion',sql.Bit, contrato?.id_dañoDevolucion ?? null)
-            .input('ubicacionEntrega',sql.Bit, contrato?.ubicacionEntrega ?? null)
-            .input('ubicacionDevolucion',sql.Bit, contrato?.ubicacionDevolucion ?? null)
+            .input('precio',sql.Float, contrato?.precio ?? '')
+            .input('fechaAlquilado',sql.Date, contrato?.fechaAlquilado ?? '')
+            .input('fechaDevolucion',sql.Date, contrato?.fechaDevolucion ?? '')
+            .input('fkCliente',sql.Int, contrato?.fkCliente ?? 0)
+            .input('fkAuto',sql.Int, contrato?.fkAuto ?? 0)
+            .input('id_dañoEntrega',sql.Int, contrato?.id_dañoEntrega ?? 0)
+            .input('id_dañoDevolucion',sql.Int, contrato?.id_dañoDevolucion ?? 0)
+            .input('ubicacionEntrega',sql.Int, contrato?.ubicacionEntrega ?? 0)
+            .input('ubicacionDevolucion',sql.Int, contrato?.ubicacionDevolucion ?? 0)
             .query(`INSERT INTO ${contratoTabla}(precio, fechaAlquilado, fechaDevolucion, fkCliente, fkAuto, id_dañoEntrega, id_dañoDevolucion, ubicacionEntrega, ubicacionDevolucion) VALUES (@precio, @fechaAlquilado, @fechaDevolucion, @fkCliente, @fkAuto, @id_dañoEntrega, @id_dañoDevolucion, @ubicacionEntrega, @ubicacionDevolucion)`);
         console.log(response)
         return response.recordset;
@@ -43,16 +43,17 @@ export class contratoService {
     updateContrato = async (id, contrato) => {
         const pool = await sql.connect(config);
         const response = await pool.request()
-        .input('precio',sql.NChar, contrato?.precio ?? '')
-        .input('fechaAlquilado',sql.Int, contrato?.fechaAlquilado ?? 0)
-        .input('fechaDevolucion',sql.NChar, contrato?.fechaDevolucion ?? '')
-        .input('fkCliente',sql.NChar, contrato?.fkCliente ?? '')
-        .input('fkAuto',sql.Bit, contrato?.fkAuto ?? null)
-        .input('id_dañoEntrega',sql.Bit, contrato?.id_dañoEntrega ?? null)
-        .input('id_dañoDevolucion',sql.Bit, contrato?.id_dañoDevolucion ?? null)
-        .input('ubicacionEntrega',sql.Bit, contrato?.ubicacionEntrega ?? null)
-        .input('ubicacionDevolucion',sql.Bit, contrato?.ubicacionDevolucion ?? null)
-            .query(`UPDATE ${autoTabla} SET patente = @patente, fkUbicacion = @fkUbicacion, disponibilidad = @disponibilidad, modelo = @modelo, limpio = @limpio WHERE Id = @Id`);
+            .input('Id',sql.Int, id)
+            .input('precio',sql.Float, contrato?.precio ?? '')
+            .input('fechaAlquilado',sql.Date, contrato?.fechaAlquilado ?? "")
+            .input('fechaDevolucion',sql.Date, contrato?.fechaDevolucion ?? "")
+            .input('fkCliente',sql.Int, contrato?.fkCliente ?? 0)
+            .input('fkAuto',sql.Int, contrato?.fkAuto ?? 0)
+            .input('id_dañoEntrega',sql.Int, contrato?.id_dañoEntrega ?? 0)
+            .input('id_dañoDevolucion',sql.Int, contrato?.id_dañoDevolucion ?? 0)
+            .input('ubicacionEntrega',sql.Int, contrato?.ubicacionEntrega ?? 0)
+            .input('ubicacionDevolucion',sql.Int, contrato?.ubicacionDevolucion ?? 0)
+            .query(`UPDATE ${contratoTabla} SET precio = @precio, fechaAlquilado = @fechaAlquilado, fechaDevolucion = @fechaDevolucion, fkCliente = @fkCliente, fkAuto = @fkAuto,id_dañoEntrega = @id_dañoEntrega,id_dañoDevolucion = @id_dañoDevolucion, ubicacionEntrega = @ubicacionEntrega, ubicacionDevolucion = @ubicacionDevolucion  WHERE Id = @Id`);
         console.log(response)
         return response.recordset;
     }
@@ -61,7 +62,7 @@ export class contratoService {
         const pool = await sql.connect(config);
         const response = await pool.request()
             .input('id',sql.Int, id)
-            .query(`DELETE FROM ${autoTabla} WHERE idAuto = @id`);
+            .query(`DELETE FROM ${contratoTabla} WHERE idContrato = @id`);
         console.log(response)
         return response.recordset;
     }
